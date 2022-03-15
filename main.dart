5d1c13abda900855b41231979b11529b4cfdd360
AhmadNHamouda <33260412+AhmadNHamouda@users.noreply.github.com>
2022-03-15 07:59:34 +0200

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,16 +1,15 @@
-import '../flutter_flow/flutter_flow_theme.dart';
-import '../flutter_flow/flutter_flow_util.dart';
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-class HomePageWidget extends StatefulWidget {
+import 'package:flutter/material.dart';
+
+
+class main extends StatefulWidget {
   const HomePageWidget({Key key}) : super(key: key);
 
   @override
-  _HomePageWidgetState createState() => _HomePageWidgetState();
+  main createState() => main();
 }
 
-class _HomePageWidgetState extends State<HomePageWidget> {
+class main extends State<HomePageWidget> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
